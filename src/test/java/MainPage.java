1d7bd717f5b3ef25de5b8ad69b74b25b84c62106
xisyn <xisyndi@gmail.ru>
2019-05-04 18:14:17 +0700

    Add login test

--- src/test/java/MainPage.java
+++ src/test/java/MainPage.java
@@ -6,6 +6,10 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class MainPage {
@@ -14,7 +18,8 @@ public class MainPage {
     @BeforeMethod
     public void setUp() {
         wd = new ChromeDriver();
-        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        wd.manage().window().maximize();
     }
 
     @Test
@@ -25,6 +30,31 @@ public class MainPage {
 
         Assert.assertEquals(menuIntem.getText(), "Ищу работу");
         Assert.assertEquals(loginBtn.getAttribute("value"), "Войти");
+    }
+
+    @Test
+    public void login() throws IOException {
+        wd.get("https://hh.ru");
+        Properties properties = new Properties();
+        FileInputStream fileInputStream;
+        fileInputStream = new FileInputStream("src/test/resources/config.propeties");
+        properties.load(fileInputStream);
+
+        String login = properties.getProperty("login");
+        String password = properties.getProperty("password");
+
+        WebElement closeRegion = wd.findElement(By.xpath("//div[@data-qa=\"notification-close-button\"]"));
+        closeRegion.click();
+
+        WebElement loginField = wd.findElement(By.xpath("//*[@class='login-input']/input[@data-qa='login-input-username']"));
+        WebElement passwordField = wd.findElement(By.xpath("//*[@class='login-input']/input[@data-qa='login-input-password']"));
+
+        loginField.sendKeys(login);
+        passwordField.sendKeys(password);
+        passwordField.submit();
+
+        WebElement profileMenu = wd.findElement(By.xpath("//span[@class='navi-item__employer-info']"));
+        Assert.assertEquals(profileMenu.getText(), "Дмитрий Хицунов");
     }
 
     @AfterMethod
